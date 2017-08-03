faaf269ee8a6edb6a6dd984090db4787b4ba808d
Vlad Faust <vladislav.faust@gmail.com>
2017-08-04 01:55:21 +0300

    feat: shrug in don't undestand message ¯\_(ツ)_/¯

--- example/responders/dont_understand.cr
+++ example/responders/dont_understand.cr
@@ -3,7 +3,7 @@ module Responders
     def respond
       R::SendMessage.new(
         chat_id: update.message.not_nil!.chat.id,
-        text: "Sorry, but I don't understand you",
+        text: "Sorry, but I don't understand you  ¯\\_(ツ)_/¯",
       )
     end
   end
