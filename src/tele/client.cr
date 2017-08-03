335919bf89224b5e8c5bdadecf705eb6af05fcfa
Vlad Faust <vladislav.faust@gmail.com>
2017-08-04 02:12:43 +0300

    ref: remove unnecessary properties

--- src/tele/client.cr
+++ src/tele/client.cr
@@ -10,15 +10,14 @@ module Tele
 
     BASE_URI = URI.parse("https://api.telegram.org")
 
-    private getter client : HTTP::Client
-    private getter token : String
+    @client : HTTP::Client
 
     def initialize(@token : String)
       @client = HTTP::Client.new(BASE_URI)
     end
 
     def make_request(method, payload, cast_to : Type.class | Nil.class | Nil = nil)
-      response = client.post("/bot" + token + "/" + method, body: payload.to_json, headers: HTTP::Headers{"Content-Type" => "application/json"})
+      response = @client.post("/bot" + @token + "/" + method, body: payload.to_json, headers: HTTP::Headers{"Content-Type" => "application/json"})
       if response.status_code == 200
         if cast_to && cast_to.class != Nil.class
           cast_to.from_json(JSON.parse(response.body.not_nil!)["result"].to_json)
