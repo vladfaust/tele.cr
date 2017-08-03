0c3f17f99d9bddde59aceb85303f767f9cf83a2e
Vlad Faust <vladislav.faust@gmail.com>
2017-08-04 01:50:26 +0300

    feat: move options parsing to bottom
    A developer may now faster understand Tele itself

--- example/bot.cr
+++ example/bot.cr
@@ -4,23 +4,6 @@ require "./actions/**"
 require "./responders/**"
 require "option_parser"
 
-token = host = ""
-port = 5000
-
-OptionParser.parse! do |parser|
-  parser.banner = "Usage: crystal tele/example/bot.cr -- -t <TOKEN> -h <HOST> -p <PORT>"
-  parser.on("-t TOKEN", "--token=TOKEN", "(Required) Telegram Bot token") { |t| token = t }
-  parser.on("-h HOST", "--host=HOST", "(Required) Host for webhooks (use ngrok.io for local development)") { |h| host = h }
-  parser.on("-p PORT", "--port=PORT", "(Optional) Local port to listen on (usually 5000)") { |p| port = p.to_i }
-  parser.on("-h", "--help", "Show this help") { puts parser }
-  parser.missing_option do |option|
-    if %w(-t -h).includes?(option)
-      puts parser
-      exit
-    end
-  end
-end
-
 class ExampleBot < Tele::Bot
   @@name = "ExampleBot"   # Used in logs
   @@color = :light_yellow # Used to colorize logs 🌈
@@ -63,6 +46,23 @@ class ExampleBot < Tele::Bot
       end
     end
     return nil
+  end
+end
+
+token = host = ""
+port = 5000
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: crystal tele/example/bot.cr -- -t <TOKEN> -h <HOST> -p <PORT>"
+  parser.on("-t TOKEN", "--token=TOKEN", "(Required) Telegram Bot token") { |t| token = t }
+  parser.on("-h HOST", "--host=HOST", "(Required) Host for webhooks (use ngrok.io for local development)") { |h| host = h }
+  parser.on("-p PORT", "--port=PORT", "(Optional) Local port to listen on (usually 5000)") { |p| port = p.to_i }
+  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.missing_option do |option|
+    if %w(-t -h).includes?(option)
+      puts parser
+      exit
+    end
   end
 end
 
